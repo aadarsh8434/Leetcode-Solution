9dfbdc1133be9703ed0ca261de979a63bb8e259e
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2025-03-06 00:00:01 +0530

    Add files via upload

--- CountTotalNumberofColoredCells.cpp
+++ CountTotalNumberofColoredCells.cpp
@@ -4,3 +4,8 @@ class Solution {
 public:
     long long coloredCells(int n) { return 1 + 4LL * n * (n - 1) / 2; }
 };
+int main() {
+    Solution s;
+    cout << s.coloredCells(3) << endl; 
+    return 0;
+}

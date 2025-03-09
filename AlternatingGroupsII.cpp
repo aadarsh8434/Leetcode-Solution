57482dce39b7ba1af1b1790c46916b3ea2df4981
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2025-03-09 19:08:49 +0530

    Add files via upload

---
+++ AlternatingGroupsII.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class solution {
+    public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+        int N = n+(k-1);
+        for(int i = 0; i < k-1; i++) {
+            colors.push_back(colors[i]);
+        }
+        int result=0;
+        int i=0;
+        int j=1;
+        while(j<N) {
+            if(colors[j]== colors[j-1]) {
+                i=j;
+                j++;
+                continue;
+            }
+            if(j-i+1==k) {
+                result++;
+                i++;
+            }
+            j++;
+        }
+        return result;
+    }
+};
+int main() {
+    solution s;
+    vector<int> colors = {1, 2,3,4,5};
+    int k=3;
+    cout<<s.numberOfAlternatingGroups(colors,k)<<endl;
+    return 0;
+
+}

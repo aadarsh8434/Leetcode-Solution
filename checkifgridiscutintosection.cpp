1eacb34912ada850e1dad625767b9dfb9ddaf7ec
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2025-03-25 23:54:39 +0530

    Add files via upload

---
+++ checkifgridiscutintosection.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        bool check(vector<vector<int>>& intervals) {
+            sort(intervals.begin(), intervals.end());
+            
+            int sections = 0;
+            int max_end = intervals[0][1];
+            
+            for (auto& interval : intervals) {
+                int start = interval[0], end = interval[1];
+                if (max_end <= start) {
+                    sections++;
+                }
+                max_end = max(max_end, end);
+            }
+            
+            return sections >= 2;
+        }
+        
+        bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+            vector<vector<int>> x_intervals, y_intervals;
+            
+            for (auto& rect : rectangles) {
+                x_intervals.push_back({rect[0], rect[2]});
+                y_intervals.push_back({rect[1], rect[3]});
+            }
+            
+            return check(x_intervals) || check(y_intervals);
+        }
+    };
+    int main() {
+        Solution s;
+        int n=4;
+        vector<vector<int>> rectangles = {{1,0,5,2}, {0,2,2,4},{3,2,5,3},{0,4,4,5}};
+        cout<<s.checkValidCuts(n,rectangles)<<endl;
+        return 0;
+    }

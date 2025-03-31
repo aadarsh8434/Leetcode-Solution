f6eb1c3f56394e243054c99f6caa47d9b4c6a8ef
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2025-03-31 11:32:07 +0530

    Add files via upload

---
+++ putmarblesinbag.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        long long putMarbles(vector<int>& weights, int k) {
+            if (k == 1) {
+                return 0;
+            }
+    
+            vector<int> pair_sums;
+            for (size_t i = 0; i < weights.size() - 1; ++i) {
+                pair_sums.push_back(weights[i] + weights[i + 1]);
+            }
+    
+            sort(pair_sums.begin(), pair_sums.end());
+    
+            long long min_score = accumulate(pair_sums.begin(), pair_sums.begin() + (k - 1), 0LL);
+            long long max_score = accumulate(pair_sums.end() - (k - 1), pair_sums.end(), 0LL);
+    
+            return max_score - min_score;
+        }
+    };
+    int main() {
+        Solution s;
+        vector<int> weights = {1, 3,5,1};
+        int k = 2;
+        cout << s.putMarbles(weights, k) << endl;
+        return 0;
+    }

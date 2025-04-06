8f264fcad31b23729e7c3500831dd5194f46e33b
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2025-04-06 21:34:49 +0530

    Add files via upload

---
+++ largestdivisiblesubset.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        vector<int> largestDivisibleSubset(vector<int>& nums) {
+            sort(nums.begin(), nums.end());
+            int n = nums.size();
+            if (n == 0) {
+                return {};
+            }
+            vector<int> dp(n, 1);
+            vector<int> parent(n, -1);
+            int max_len = 1;
+            int max_index = 0;
+    
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    if (nums[i] % nums[j] == 0) {
+                        if (dp[j] + 1 > dp[i]) {
+                            dp[i] = dp[j] + 1;
+                            parent[i] = j;
+                            if (dp[i] > max_len) {
+                                max_len = dp[i];
+                                max_index = i;
+                            }
+                        }
+                    }
+                }
+            }
+    
+            vector<int> result;
+            int curr = max_index;
+            while (curr != -1) {
+                result.push_back(nums[curr]);
+                curr = parent[curr];
+            }
+            return result;
+        }
+    };
+    int main() {
+        Solution s;
+        vector<int> nums = {1, 2, 4, 8};
+        vector<int> result = s.largestDivisibleSubset(nums);
+        cout << "Largest Divisible Subset: ";
+        for (int num : result) {
+            cout << num << " ";
+            }
+            cout << endl;
+            return 0;
+    }

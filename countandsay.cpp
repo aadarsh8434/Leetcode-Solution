abafb8a0d1e3f741a617805d62e257601655ad2c
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2025-04-18 22:56:45 +0530

    Add files via upload

---
+++ countandsay.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        string countAndSay(int n) {
+            string res = "1";
+            helper(1, n, res);
+            return res;
+        }
+    
+        void helper(int ind, int n, string& res) {
+            if (ind >= n)
+                return;
+            string prev_val = res;
+            res = "";
+            int left = 0, count = 0;
+            for (int right = 0; right < prev_val.size(); right++) {
+                if (prev_val[left] != prev_val[right]) {
+                    res += to_string(count) + prev_val[left];
+                    left = right;
+                    count = 1;
+                } else {
+                    count++;
+                }
+            }
+            res += to_string(count) + prev_val[left];
+            return helper(ind + 1, n, res);
+        }
+    };
+    int main() {
+        Solution s;
+        cout << s.countAndSay(1) << endl; 
+        cout << s.countAndSay(2) << endl;
+        cout << s.countAndSay(3) << endl;
+        return 0;
+    }
